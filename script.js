bbaaf4fb60a615c113ddcf2465abebfa0e97e3ba
taehasan <taehasan@iu.edu>
2024-08-04 03:29:53 -0400

    Taemoor Website First push

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/* reponsive mobile menu*/
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+        
+
+
+}
+
